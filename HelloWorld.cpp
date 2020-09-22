e221a78b6a708cf96dc8a454ce261efc4a8469f8
Yan Yang <gx0225@wayne.edu>
2020-09-21 20:08:47 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+//Author: Yan Yang <gx0225@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
